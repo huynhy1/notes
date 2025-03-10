74868f6b089aa6452410803e596827923902a75d
huynhy1 <ericoxo89@gmail.com>
2025-03-10 00:31:44 -0700

    List, Update, and Delete notes

---
+++ packages/functions/src/update.ts
@@ -0,0 +1,28 @@
+import * as uuid from "uuid";
+import { Resource } from "sst";
+import { Util } from "@notes/core/util";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { UpdateCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export const main = Util.handler(async (event) => {
+  let data = JSON.parse(event.body || "{}");
+
+  const params = {
+    TableName: Resource.Notes.name,
+    Key: {
+      userId: "123", // The id of the author
+      noteId: event?.pathParameters?.id, // The id of the note from the path
+    },
+    UpdateExpression: "SET content = :content, attachment = :attachment",
+    ExpressionAttributeValues: {
+      ":content": data.content || null,
+      ":attachment": data.attachment || null
+    }
+  };
+
+  await dynamoDb.send(new UpdateCommand(params));
+
+  return JSON.stringify({ status: true });
+});
